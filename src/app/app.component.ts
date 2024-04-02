89f1626ea73191b48993070c4522b8f03ccf2883
Alijaved07 <alijavedofficial0007@gmail.com>
2024-04-03 00:20:33 +0500

    Applied browser animations for loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('showLogo', [
+      state('visible', style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      state('hidden', style({
+        opacity: 0,
+        transform: 'scale(0.5)'
+      })),
+      transition('visible => hidden', [
+        animate('2s')
+      ])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mealmate';
+
+  showLogo: boolean = true;
+
+  ngOnInit() {
+    // Hide the logo after 2 seconds
+    setTimeout(() => {
+      this.showLogo = false;
+    }, 2000);
+  }
 }
